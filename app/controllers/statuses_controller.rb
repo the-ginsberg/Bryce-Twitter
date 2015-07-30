81869f98c0037f350147e8405e9c8ee558553cf7
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-07-30 11:52:29 -0400

    pagination for statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,12 @@
 class StatusesController < ApplicationController
   def index
-    @statuses = Status.all
+    @page = params[:page].to_i || 1
+    offset = (@page -1) * 25
+    @statuses = Status.
+    order(created_at: :desc).
+    limit(25).
+    offset(offset).
+    all
   end
 
   def show
